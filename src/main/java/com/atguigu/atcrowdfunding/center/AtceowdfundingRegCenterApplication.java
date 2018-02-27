96f5ec22c6c3fcbf5bf4fce765f938236901278e
luwan <344809958@qq.com>
2018-02-27 19:10:14 +0800

    提交项目
    Change-Id: I5a0720da6e8da0e7089e433dbdf6f60f311bef38

---
+++ src/main/java/com/atguigu/atcrowdfunding/center/AtceowdfundingRegCenterApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.atcrowdfunding.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class AtceowdfundingRegCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtceowdfundingRegCenterApplication.class, args);
+	}
+}
